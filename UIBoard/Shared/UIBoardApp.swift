9fe16da5080a2cd6032fbfb8000ee265b2e0fec2
Damiaan Dufaux <damiaan@dpro.be>
2021-07-01 17:31:13 +0200

    Add SwiftUI project

---
+++ UIBoard/Shared/UIBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIBoardApp.swift
+//  Shared
+//
+//  Created by Damiaan on 01/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct UIBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
